234e0a9898412a75b3650003782e7d49fc4c7f5f
miroslawmajka <go2mirek@gmail.com>
2020-10-08 22:31:03 +0100

    Added TypeScript and local dev environment

--- README.md
+++ README.md
@@ -8,22 +8,22 @@ Deployments done with [Serverless](https://www.serverless.com/).
 
 ## Setup
 
-1. Install **Serverless**:
-
-```bash
-npm install -g serverless
-```
-
-2. Install dependencies:
+1. Install dependencies:
 
 ```bash
 npm install
 ```
 
+2. Run locally:
+
+```bash
+npm start
+```
+
 3. Run deployment to AWS:
 
 ```bash
-serverless deploy
+npm run deploy
 ```
 
 4. Navigate to the GraphQL Playground available at https://RANDOM-ID.execute-api.eu-west-1.amazonaws.com/dev/graphql.
---
+++ src/graphql.ts
@@ -0,0 +1,64 @@
+import { ApolloServer, gql } from 'apollo-server-lambda';
+
+// TODO: refactor the type definitions and the resolvers out of here
+const typeDefs = gql`
+  type Query {
+    hello: String
+    nodeVersion: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+    nodeVersion: () => process.version
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ event, context }) => ({
+    headers: event.headers,
+    functionName: context.functionName,
+    event,
+    context
+  }),
+  playground: {
+    endpoint: '/dev/graphql'
+  }
+});
+
+// TODO: refactor this to use async/await
+async function runApollo(event: any, context: any, apollo: any) {
+  return new Promise((resolve, reject) => {
+    const callback = (error: any, body: any) => (error ? reject(error) : resolve(body));
+
+    apollo(event, context, callback);
+  });
+}
+
+exports.graphqlHandler = async (event, context, cb) => {
+  const apollo = server.createHandler({
+    cors: {
+      origin: '*',
+      methods: 'POST',
+      allowedHeaders: ['Content-Type', 'Origin', 'Accept']
+    }
+  });
+
+  // Local development ONLY
+  event.httpMethod = event.httpMethod || 'POST';
+  event.headers = {
+    'content-type': 'application/json',
+    ...(event.headers || {})
+  };
+
+  try {
+    const response = await runApollo(event, context, apollo);
+
+    return response;
+  } catch (err) {
+    cb(err, null);
+  }
+};
